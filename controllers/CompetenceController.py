c9379c8a0a46cdb2da8477b5908e5c893dbd89cf
German Gomez <germandg99@gmail.com>
2026-04-26 22:25:40 -0500

    edit Competences ui to allow re-size the window with internal components

--- controllers/CompetenceController.py
+++ controllers/CompetenceController.py
@@ -8,7 +8,7 @@ from managers.GroupAthleteManager import GroupAthleteManager
 from models.Competence import Competence
 from models.Group import Group
 from utils.Validations import *
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from utils.style_sheet import ButtonStyleSheet
 
 
@@ -20,10 +20,185 @@ class CompetenceController:
         self.groups_controller = None
         self.take_time_window = TakeTimeWindow()
         self.take_time_controller = None
+        self.setup_resizable_layout()
         self.clear_tables()
         self.set_style_sheet()
         self.message_box = None
         self.built_message_box()
+
+    def setup_resizable_layout(self):
+        # 1. Main Setup
+        self.centralwidget = QtWidgets.QWidget(self.window)
+        self.main_layout = QtWidgets.QHBoxLayout(self.centralwidget) # Horizontal split
+        self.main_layout.setContentsMargins(20, 20, 20, 20)
+        self.main_layout.setSpacing(30)
+
+        # 2. Competence Column (Left)
+        self.setup_competence_column()
+
+        # 3. Tandas Column (Right)
+        self.setup_tandas_column()
+
+        self.window.setCentralWidget(self.centralwidget)
+        self.window.setMinimumSize(QtCore.QSize(1300, 750))
+
+    def setup_competence_column(self):
+        self.left_column = QtWidgets.QVBoxLayout()
+        self.left_column.setSpacing(10)
+
+        # Centering Wrapper for the Form
+        self.comp_centering_layout = QtWidgets.QHBoxLayout()
+        
+        # Form Container (Fixed width to match original design)
+        self.comp_form_container = QtWidgets.QWidget()
+        self.comp_form_container.setFixedWidth(740)
+        self.comp_form_layout = QtWidgets.QVBoxLayout(self.comp_form_container)
+        self.comp_form_layout.setContentsMargins(0, 0, 0, 0)
+        self.comp_form_layout.setSpacing(5)
+
+        # Header
+        self.window.lb_title.setParent(self.comp_form_container)
+        self.window.lb_title.setMinimumSize(QtCore.QSize(700, 40))
+        self.comp_form_layout.addWidget(self.window.lb_title)
+
+        # Inputs Grid
+        self.comp_inputs_grid = QtWidgets.QGridLayout()
+        self.comp_inputs_grid.setSpacing(10)
+        
+        # Row 1: Nombre & Lugar
+        self.window.label_2.setParent(self.comp_form_container)
+        self.window.label_3.setParent(self.comp_form_container)
+        self.window.ed_name.setParent(self.comp_form_container)
+        self.window.ed_place.setParent(self.comp_form_container)
+        
+        self.comp_inputs_grid.addWidget(self.window.label_2, 0, 0, QtCore.Qt.AlignCenter)
+        self.comp_inputs_grid.addWidget(self.window.label_3, 0, 1, QtCore.Qt.AlignCenter)
+        self.comp_inputs_grid.addWidget(self.window.ed_name, 1, 0)
+        self.comp_inputs_grid.addWidget(self.window.ed_place, 1, 1)
+
+        # Errors Row 1
+        self.window.lb_name_error.setParent(self.comp_form_container)
+        self.window.lb_place_error.setParent(self.comp_form_container)
+        self.comp_inputs_grid.addWidget(self.window.lb_name_error, 2, 0)
+        self.comp_inputs_grid.addWidget(self.window.lb_place_error, 2, 1)
+
+        # Row 2: Fecha & Hora
+        self.window.label_4.setParent(self.comp_form_container)
+        self.window.label_5.setParent(self.comp_form_container)
+        self.window.ed_date.setParent(self.comp_form_container)
+        self.window.ed_time.setParent(self.comp_form_container)
+        self.comp_inputs_grid.addWidget(self.window.label_4, 3, 0, QtCore.Qt.AlignCenter)
+        self.comp_inputs_grid.addWidget(self.window.label_5, 3, 1, QtCore.Qt.AlignCenter)
+        self.comp_inputs_grid.addWidget(self.window.ed_date, 4, 0)
+        self.comp_inputs_grid.addWidget(self.window.ed_time, 4, 1)
+
+        # Errors Row 2 & Label 6 (año-mes-dia)
+        self.window.lb_date_error.setParent(self.comp_form_container)
+        self.window.lb_time_error.setParent(self.comp_form_container)
+        self.window.label_6.setParent(self.comp_form_container)
+        self.comp_inputs_grid.addWidget(self.window.lb_date_error, 5, 0)
+        self.comp_inputs_grid.addWidget(self.window.label_6, 6, 0, QtCore.Qt.AlignCenter)
+        self.comp_inputs_grid.addWidget(self.window.lb_time_error, 5, 1)
+        
+        self.comp_form_layout.addLayout(self.comp_inputs_grid)
+
+        # Buttons Area
+        self.comp_btns_layout = QtWidgets.QHBoxLayout()
+        self.window.btn_create_competition.setParent(self.comp_form_container)
+        self.window.btn_get_all_competitions.setParent(self.comp_form_container)
+        self.window.btn_create_competition.setMinimumSize(QtCore.QSize(170, 40))
+        self.window.btn_get_all_competitions.setMinimumSize(QtCore.QSize(160, 40))
+        self.comp_btns_layout.addStretch()
+        self.comp_btns_layout.addWidget(self.window.btn_create_competition)
+        self.comp_btns_layout.addStretch()
+        self.comp_btns_layout.addWidget(self.window.btn_get_all_competitions)
+        self.comp_btns_layout.addStretch()
+        self.comp_form_layout.addLayout(self.comp_btns_layout)
+
+        # Finalize Form centering
+        self.comp_centering_layout.addStretch()
+        self.comp_centering_layout.addWidget(self.comp_form_container)
+        self.comp_centering_layout.addStretch()
+        self.left_column.addLayout(self.comp_centering_layout)
+
+        # Table
+        self.window.competences_table.setParent(self.centralwidget)
+        header = self.window.competences_table.horizontalHeader()
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch) # Nombre
+        for col in range(1, 8):
+            header.setSectionResizeMode(col, QtWidgets.QHeaderView.Interactive)
+            self.window.competences_table.setColumnWidth(col, 100)
+        self.left_column.addWidget(self.window.competences_table)
+        
+        # Bottom error label
+        self.window.lb_error_delete.setParent(self.centralwidget)
+        self.left_column.addWidget(self.window.lb_error_delete)
+
+        self.main_layout.addLayout(self.left_column)
+
+    def setup_tandas_column(self):
+        self.right_column = QtWidgets.QVBoxLayout()
+        self.right_column.setSpacing(10)
+
+        # Centering Wrapper for Tandas Form
+        self.tan_centering_layout = QtWidgets.QHBoxLayout()
+        self.tan_form_container = QtWidgets.QWidget()
+        self.tan_form_container.setFixedWidth(500)
+        self.tan_form_layout = QtWidgets.QVBoxLayout(self.tan_form_container)
+        self.tan_form_layout.setContentsMargins(0, 0, 0, 0)
+        self.tan_form_layout.setSpacing(10)
+
+        # Header & Subtitle
+        self.window.lb_subtitle.setParent(self.tan_form_container)
+        self.window.lb_competence_selected.setParent(self.tan_form_container)
+        self.tan_form_layout.addWidget(self.window.lb_subtitle)
+        self.tan_form_layout.addWidget(self.window.lb_competence_selected)
+
+        # Inputs Grid
+        self.tan_grid = QtWidgets.QGridLayout()
+        self.window.label_8.setParent(self.tan_form_container)
+        self.window.label_9.setParent(self.tan_form_container)
+        self.window.ed_group_name.setParent(self.tan_form_container)
+        self.window.ed_group_order.setParent(self.tan_form_container)
+        self.window.btn_create_group.setParent(self.tan_form_container)
+        
+        self.tan_grid.addWidget(self.window.label_8, 0, 0, QtCore.Qt.AlignCenter)
+        self.tan_grid.addWidget(self.window.label_9, 0, 1, QtCore.Qt.AlignCenter)
+        self.tan_grid.addWidget(self.window.ed_group_name, 1, 0)
+        self.tan_grid.addWidget(self.window.ed_group_order, 1, 1)
+        self.tan_grid.addWidget(self.window.btn_create_group, 1, 2)
+        
+        # Errors
+        self.window.lb_group_name_error.setParent(self.tan_form_container)
+        self.window.lb_group_order_error.setParent(self.tan_form_container)
+        self.tan_grid.addWidget(self.window.lb_group_name_error, 2, 0)
+        self.tan_grid.addWidget(self.window.lb_group_order_error, 2, 1)
+        
+        self.tan_form_layout.addLayout(self.tan_grid)
+
+        # Actions
+        self.tan_actions = QtWidgets.QHBoxLayout()
+        self.window.btn_see_all_groups.setParent(self.tan_form_container)
+        self.tan_actions.addWidget(self.window.btn_see_all_groups)
+        self.tan_actions.addStretch()
+        self.tan_form_layout.addLayout(self.tan_actions)
+
+        # Finalize Tandas Form centering
+        self.tan_centering_layout.addStretch()
+        self.tan_centering_layout.addWidget(self.tan_form_container)
+        self.tan_centering_layout.addStretch()
+        self.right_column.addLayout(self.tan_centering_layout)
+
+        # Table
+        self.window.groups_table.setParent(self.centralwidget)
+        header = self.window.groups_table.horizontalHeader()
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch) # Nombre
+        for col in range(1, 5):
+            header.setSectionResizeMode(col, QtWidgets.QHeaderView.Interactive)
+            self.window.groups_table.setColumnWidth(col, 80)
+        self.right_column.addWidget(self.window.groups_table)
+
+        self.main_layout.addLayout(self.right_column)
 
     def built_message_box(self):
         self.message_box = QtWidgets.QMessageBox(self.window)
